a352c6eaf684c4b6d8fd068bd82520cf43acd531
Owen Leather <43189206+OLeather@users.noreply.github.com>
2020-05-18 14:08:04 -0700

    added more 3d models

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,8 +17,7 @@ public class Main extends TimerTask implements KeyListener {
     private boolean upRotPressed = false;
     private boolean downRotPressed = false;
     private double camMoveSpeed = 0.5;
-    private double camRotateSpeed = 0.02;
-
+    private double camRotateSpeed = 0.05;
     public Main() {
         init();
     }
@@ -51,6 +50,15 @@ public class Main extends TimerTask implements KeyListener {
         //File from http://web.mit.edu/djwendel/www/weblogo/shapes/basic-shapes/sphere/sphere.obj
         String sphereFile = "sphere.obj";
 
+        //File from https://github.com/kivy/kivy/blob/master/examples/3Drendering/monkey.obj (blender monkey object)
+        String monkeyFile = "monkey.obj";
+
+        //File from https://sketchfab.com/3d-models/alduin-c1d54a5145004198ac9c45da418239c8
+        String dragonFile = "alduin.obj";
+
+        //File from https://www.cgtrader.com/items/838998/download-page
+        String carFile = "Ford_GT.obj";
+        
         //Create a series of test objects
         Object3D triObj1 =
                 new Object3D(new Tri3D[]{new Tri3D(new Point3D(0, 0, 0), new Point3D(5, 5, 0), new Point3D(0, 5, 0))},
@@ -59,7 +67,10 @@ public class Main extends TimerTask implements KeyListener {
                 new Object3D(new Tri3D[]{new Tri3D(new Point3D(0, 0, 0), new Point3D(5, 5, 0), new Point3D(0, 5, 0))},
                         "Test OBJ1", Color.blue);
         Object3D cubeObject = Object3D.fromOBJFile(cubeFile, "cube", Color.GREEN);
-        Object3D sphereObject = Object3D.fromOBJFile(sphereFile, "sphere", Color.RED);
+        Object3D sphereObject = Object3D.fromOBJFile(sphereFile, "sphere", Color.BLUE);
+        Object3D monkeyObject = Object3D.fromOBJFile(monkeyFile, "monkey", Color.RED);
+        Object3D dragonObject = Object3D.fromOBJFile(dragonFile, "dragon", Color.RED);
+        Object3D carObject = Object3D.fromOBJFile(carFile, "car", Color.RED);
 
         //Adjust positions and rotations of test objects
         triObj1.setPosition(new Point3D(0, 0, 10));
@@ -72,8 +83,21 @@ public class Main extends TimerTask implements KeyListener {
 
         sphereObject.setPosition(new Point3D(0, 0, 20));
 
+        monkeyObject.setPosition(new Point3D(0, 0, 20));
+        monkeyObject.setRotationDegrees(new Point3D(180, 180, 0));
+
+        dragonObject.setPosition(new Point3D(0, 0, 1000));
+        dragonObject.setScale(new Point3D(.01, .01, .01));
+        dragonObject.setRotationDegrees(new Point3D(180, 0, 0));
+
+        carObject.setPosition(new Point3D(0, 0, 500));
+        carObject.setScale(new Point3D(0.01, 0.01, 0.01));
+        carObject.setRotationDegrees(new Point3D(180, 180, 0));
+
+
+
         //Add objects to the renderer
-        Renderer.getInstance().addObject(sphereObject);
+        Renderer.getInstance().addObject(spaceshipObject);
 
         //Initial object rendering
         Renderer.getInstance().renderObjects();
